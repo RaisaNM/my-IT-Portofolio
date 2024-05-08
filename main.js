f062f5b71b3cb17306daf04766606e1d4859f18e
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2024-05-08 16:49:24 +0200

    added new style

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function showSection(id) {
     }
     document.getElementById(id).style.display = 'block';
 }
+
+function showProject(id) {
+    var projects = document.getElementsByClassName('project');
+    for (var i = 0; i < projects.length; i++) {
+        projects[i].style.display = 'none';
+    }
+    document.getElementById(id).style.display = 'block';
+}
+
+
